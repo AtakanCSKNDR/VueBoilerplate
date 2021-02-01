693c5b2b56a92489c0b04077965a38d84af2fd87
Atakan Cevat COŞKUNDERE <a.cevat77@gmail.com>
2021-02-01 15:42:23 +0300

    Router has been changed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 
 Vue.use(VueRouter);
@@ -9,11 +8,6 @@ import base from "@/router/modules/base";
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/Login",
     name: "Login",
     component: Login,
   },
